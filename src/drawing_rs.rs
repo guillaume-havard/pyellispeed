c49db138922bb813b9cb43c3c11d55dcb353a26b
Guillaume Havard <guillaumeh@anotherbrain.ai>
2024-12-19 17:46:53 +0100

    refacto(drawing): Speed up the make_ellipsoid_image function
    Name (time in ms)                          Min                Max               Mean            StdDev             Median               IQR            Outliers      OPS            Rounds  Iterations
    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    test_bench_make_ellipsoid_image_rs     32.6350 (1.0)      39.3704 (1.0)      33.5218 (1.0)      1.5279 (1.0)      33.0624 (1.0)      0.6873 (1.0)           1;2  29.8314 (1.0)          19           1
    test_bench_make_ellipsoid_image        55.9036 (1.71)     67.3712 (1.71)     57.5604 (1.72)     2.6305 (1.72)     56.6892 (1.71)     1.3802 (2.01)          2;2  17.3731 (0.58)         19           1
    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    
    I use a more mathematical way to implement the function to be sure to understand each step.
    I reviewed output to minimize copy time.

--- src/drawing_rs.rs
+++ src/drawing_rs.rs
@@ -1,70 +1,43 @@
 use super::geometry_rs;
-use ndarray::prelude::*;
-use ndarray::Array3;
-use numpy::{IntoPyArray, PyArray3};
+use ndarray::{Array1, Zip};
+use numpy::{PyArray3, PyArrayMethods};
 use pyo3::prelude::*;
 
-fn make_ellipsoid_image(
+pub fn make_ellipsoid_image<'py>(
+    py: Python<'py>,
     shape: [usize; 3],
     center: [f64; 3],
     radii: [f64; 3],
     angles: [f64; 3],
-) -> Array3<u8> {
-    let neg_angles: [f64; 3] = [-angles[0], -angles[1], -angles[2]];
-    let rotation_matrix =
-        geometry_rs::build_rotation_matrix(neg_angles[0], neg_angles[1], neg_angles[2], Some(false));
+) -> Bound<'py, PyArray3<bool>> {
+    let rotation = geometry_rs::build_rotation_matrix(angles[0], angles[1], angles[2], Some(false));
 
-    // Create the grid for all three dimensions
-    let xi: Vec<Array1<f64>> = shape
-        .iter()
-        .map(|&s| Array::linspace(0.0, s as f64 - 1.0, s).mapv(|v| v - (s as f64 / 2.0).floor()))
-        .collect();
+    // Generate a 3D grid of coordinates
+    let ellipsoid = PyArray3::<bool>::zeros(py, shape, false);
+    let (cx, cy, cz) = (center[0], center[1], center[2]);
+    let radii = Array1::from(vec![radii[0], radii[1], radii[2]]);
+    // let radii_sq = Array1::from_vec(vec![radii[0]*radii[0], radii[1]*radii[1], radii[2]*radii[2]]);
 
-    // Create the meshgrid as a flat matrix
-    let grid_points = Array::from_shape_fn((3, shape[0] * shape[1] * shape[2]), |(dim, idx)| {
-        let z = idx / (shape[1] * shape[2]);
-        let y = (idx / shape[2]) % shape[1];
-        let x = idx % shape[2];
-        let coord = match dim {
-            0 => xi[2][x],
-            1 => xi[1][y],
-            2 => xi[0][z],
-            _ => unreachable!(),
-        };
-        coord
-    });
+    // This is safe because the PyArray `ellipsoid` is already allocated in Python's memory,
+    // and the returned reference is tied to the same lifetime as py (the Python interpreter's lifetime).
+    unsafe {
+        let mut ellipsoid_mut = ellipsoid.as_array_mut();
 
-    // Rotate the grid points
-    let rotated_points = rotation_matrix.dot(&grid_points);
+        Zip::indexed(&mut ellipsoid_mut).par_for_each(|(z, y, x), val| {
+            let coords = Array1::from_vec(vec![x as f64 - cx, y as f64 - cy, z as f64 - cz]);
 
-    // Compute the grid center in rotated space
-    let grid_center: Array1<f64> = center
-        .iter()
-        .zip(shape.iter().rev())
-        .map(|(&c, &s)| c - (s as f64 / 2.0))
-        .collect::<Array1<f64>>()
-        .dot(&rotation_matrix);
+            let coords_prime = rotation.dot(&coords);
 
-    // Compute the ellipsoid mask in parallel
-    let radii_sq: Array1<f64> = radii.iter().rev().map(|&r| r * r).collect();
-    let grid_center_rev: Array1<f64> = grid_center.iter().rev().cloned().collect();
+            // equation = x_prime.powi(2) / rx.powi(2) + y_prime.powi(2) / ry.powi(2) + z_prime.powi(2) / rz.powi(2);
+            let equation = &coords_prime.mapv(|x| x * x) / &radii.mapv(|x| x * x);
+            let equation = equation.sum();
 
-    let mut ellipsoid = Array3::<u8>::zeros(shape);
-    let shape_prod = shape[1] * shape[2];
+            // Check if the point is on the ellipsoid
+            *val = equation == 1.0;
+        });
 
-    // Use par_map_inplace to modify the array in parallel
-    ellipsoid.indexed_iter_mut().for_each(|((z, y, x), val)| {
-        let idx = z * shape_prod + y * shape[2] + x;
-        let dx = rotated_points[[2, idx]] - grid_center_rev[0];
-        let dy = rotated_points[[1, idx]] - grid_center_rev[1];
-        let dz = rotated_points[[0, idx]] - grid_center_rev[2];
-        let value = (dx * dx) / radii_sq[0] + (dy * dy) / radii_sq[1] + (dz * dz) / radii_sq[2];
-        if value <= 1.0 {
-            *val = 1;
-        }
-    });
-
-    ellipsoid
+        ellipsoid
+    }
 }
 
 #[pymodule]
@@ -77,10 +50,9 @@ pub fn drawing_rs<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
         center: [f64; 3],
         radii: [f64; 3],
         angles: [f64; 3],
-    ) -> Bound<'py, PyArray3<u8>> {
-        make_ellipsoid_image(shape, center, radii, angles).into_pyarray(py)
+    ) -> Bound<'py, PyArray3<bool>> {
+        make_ellipsoid_image(py, shape, center, radii, angles)
     }
-
     Ok(())
 }
 
@@ -95,23 +67,27 @@ mod tests {
         let radii_xyz = [5., 10., 30.];
         let angles_xyz = [0., 0., 0.];
 
-        let image = make_ellipsoid_image(shape, center_xyz, radii_xyz, angles_xyz);
-        assert_eq!(image.shape(), shape);
-        let nonzero_count = image.iter().filter(|&&x| x > 0).count();
-        assert_ne!(nonzero_count, 0);
+        let image = make_ellipsoid_image(Py, shape, center_xyz, radii_xyz, angles_xyz);
+        unsafe {
+            let image = image.as_array();
+        
+            assert_eq!(image.shape(), shape);
+            let nonzero_count = image.iter().filter(|&&x| x).count();
+            assert_ne!(nonzero_count, 0);
 
-        assert_eq!(image[[20, 50, 50]], 1);
-        assert_eq!(image[[80, 50, 50]], 1);
-        assert_eq!(image[[50, 40, 50]], 1);
-        assert_eq!(image[[50, 60, 50]], 1);
-        assert_eq!(image[[50, 50, 45]], 1);
-        assert_eq!(image[[50, 50, 55]], 1);
+            assert_eq!(image[[20, 50, 50]], true);
+            assert_eq!(image[[80, 50, 50]], true);
+            assert_eq!(image[[50, 40, 50]], true);
+            assert_eq!(image[[50, 60, 50]], true);
+            assert_eq!(image[[50, 50, 45]], true);
+            assert_eq!(image[[50, 50, 55]], true);
 
-        //assert_eq!(image[[50, 50, 54]] , 0);
-        assert_eq!(image[[50, 50, 56]], 0);
-        assert_eq!(image[[50, 49, 55]], 0);
-        assert_eq!(image[[50, 51, 55]], 0);
-        assert_eq!(image[[49, 50, 55]], 0);
-        assert_eq!(image[[51, 50, 55]], 0);
+            //assert_eq!(image[[50, 50, 54]] , false);
+            assert_eq!(image[[50, 50, 56]], false);
+            assert_eq!(image[[50, 49, 55]], false);
+            assert_eq!(image[[50, 51, 55]], false);
+            assert_eq!(image[[49, 50, 55]], false);
+            assert_eq!(image[[51, 50, 55]], false);
+        }
     }
 }
